373de6df25990a73ddfb80163b7d2df38e4b7157
Pasquale D'Silva <accounts@psql.me>
2023-09-06 15:18:05 -0400

    make image dynamic, powered by a val that updates every 15 mins...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import logo from './logo.svg';
+
 
 function App() {
+  const [backgroundImageUrl, setBackgroundImageUrl] = useState("");
+
+  useEffect(() => {
+    // Fetch the background image URL
+    fetch("https://api.val.town/v1/run/pasquale.labImageEndpoint")
+      .then((response) => response.text())
+      .then((imageUrl) => {
+        // Remove quotes from the fetched image URL
+        imageUrl = imageUrl.replace(/^"|"$/g, "");
+        setBackgroundImageUrl(imageUrl);
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+  }, []);
+
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <img className='m-auto' src='logo.svg' alt="Lingonberry Intelligence" />
+    <div className="App" style={{ backgroundImage: `url('${backgroundImageUrl}')` }}>
+      {/* Rest of your App content here */}
+      <div className="info">
+        <img src={logo} className="logo" alt="logo" />
+        <span>Lingonberry Intelligence</span> 
+      </div>
     </div>
   );
 }
